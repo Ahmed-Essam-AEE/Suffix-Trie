d040b41f8ebf2bf4b60d6696d57d7f04ed091d9b
ahmed-essam-ai <60892629+ahmed-essam-ai@users.noreply.github.com>
2021-12-21 23:29:56 +0200

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
-struct Node
-        {
+class trieNode;
+class LinkedList;
+struct Node{
+
     char character;
-    Node *next;
+    trieNode *next;
+
         };
+class trieNode
+{
+public:
+    LinkedList list;
+    int index;
+
+};
 class LinkedList{
 private:
     Node* head ,* tail;
@@ -15,19 +25,9 @@ public:
         this->head=nullptr;
         this->tail=nullptr;
     }
-    
 
-    void printList()
-    {
-        Node *tmp= this->head;
-        while (tmp != this->tail)
-        {
-           cout<<tmp->character<<" ";
-           tmp=tmp->next;
-        }
-        cout<<tmp->character;
-        cout<<endl;
-    }
+
+
     void push_back(char value)
     {
         Node*tmp=new Node();
@@ -41,19 +41,32 @@ public:
         }
         else
         {
-            this->tail->next=tmp;
+            this->tail->next=tmp->next;
             this->tail=tmp;
         }
+
+    }
+    void printList()
+    {
+        Node *tmp= this->head;
+        while (tmp != this->tail)
+        {
+            cout<<tmp->character<<" ";
+            tmp=tmp->next->list->head;
+        }
+        cout<<tmp->character;
+        cout<<endl;
     }
         };
+
 int main() {
-    LinkedList *list=new LinkedList();
-    list->push_back('a');
-    list->push_back('h');
-    list->push_back('m');
-    list->push_back('e');
-    list->push_back('d');
-    list->printList();
+    trieNode *tn=new trieNode();
+    tn->list.push_back('a');
+    tn->list.push_back('h');
+    tn->list.push_back('m');
+    tn->list.push_back('e');
+    tn->list.push_back('d');
+    tn->list.printList();
 
     return 0;
 }
